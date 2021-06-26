c726ebd95fab903f9fdbe7a077abd87ec2c1bdb2
Kirill Kuznetsov <kirill.desirehd@gmail.com>
2021-06-26 22:33:59 +0300

    Try to autoupdate materialization

--- update.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-set -euo pipefail
-
-nix build -j8 -f default.nix hls-renamed --no-link --argstr ghcVersion ghc865 --arg checkMaterialization true 2>&1 | grep -om1 '/nix/store/.*-updateMaterialized' | bash
-nix build -j8 -f default.nix hls-renamed --no-link --argstr ghcVersion ghc884 --arg checkMaterialization true 2>&1 | grep -om1 '/nix/store/.*-updateMaterialized' | bash
-nix build -j8 -f default.nix hls-renamed --no-link --argstr ghcVersion ghc8104 --arg checkMaterialization true 2>&1 | grep -om1 '/nix/store/.*-updateMaterialized' | bash
-nix build -j8 -f default.nix hls-renamed --no-link --argstr ghcVersion ghc8105 --arg checkMaterialization true 2>&1 | grep -om1 '/nix/store/.*-updateMaterialized' | bash
-nix build -j8 -f default.nix hls-renamed --no-link --argstr ghcVersion ghc901 --arg checkMaterialization true 2>&1 | grep -om1 '/nix/store/.*-updateMaterialized' | bash
