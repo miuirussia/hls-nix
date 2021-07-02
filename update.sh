ff3432422caec4daf795f1c606e4e3e6480a4abd
Kirill Kuznetsov <kirill.desirehd@gmail.com>
2021-07-02 09:24:26 +0300

    Try

--- update.sh
+++ update.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-nix-build -A project.plan-nix.passthru.updateMaterialized --argstr ghcVersion ghc865 --arg checkMaterialization true 2>&1 | tee >( grep -om1 '/nix/store/.*-updateMaterialized' | bash )
-nix-build -A project.plan-nix.passthru.updateMaterialized --argstr ghcVersion ghc884 --arg checkMaterialization true 2>&1 | tee >( grep -om1 '/nix/store/.*-updateMaterialized' | bash )
-nix-build -A project.plan-nix.passthru.updateMaterialized --argstr ghcVersion ghc8105 --arg checkMaterialization true 2>&1 | tee >( grep -om1 '/nix/store/.*-updateMaterialized' | bash )
+$(nix-build -A project.plan-nix.passthru.updateMaterialized --argstr ghcVersion ghc865)
+$(nix-build -A project.plan-nix.passthru.updateMaterialized --argstr ghcVersion ghc884)
+$(nix-build -A project.plan-nix.passthru.updateMaterialized --argstr ghcVersion ghc8105)
