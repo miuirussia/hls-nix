89e853fb6448b7ff16360c93f4b33fc5b279e2fa
miuirussia <miuirussia@users.noreply.github.com>
2021-08-05 03:53:26 +0000

    Automatic materialization update – 2021-08-05 03:23:36

--- materialized-macOS/hls-unstable-ghc8104/.plan.nix/hls-plugin-api.nix
+++ materialized-macOS/hls-unstable-ghc8104/.plan.nix/hls-plugin-api.nix
@@ -11,7 +11,7 @@
     flags = { pedantic = false; };
     package = {
       specVersion = "2.4";
-      identifier = { name = "hls-plugin-api"; version = "1.2.0.0"; };
+      identifier = { name = "hls-plugin-api"; version = "1.2.0.1"; };
       license = "Apache-2.0";
       copyright = "The Haskell IDE Team";
       maintainer = "alan.zimm@gmail.com";
