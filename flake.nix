32d6becebf594d662e1327188f5f0f5b2dc4afce
Kirill Kuznetsov <kirill.desirehd@gmail.com>
2021-09-08 21:03:01 +0300

    Optimize

--- flake.nix
+++ flake.nix
@@ -29,7 +29,7 @@
     flake-compat.flake = false;
   };
 
-  outputs = inputs @ { nixpkgs, haskell-nix, ... }:
+  outputs = inputs @ { nixpkgs, haskell-nix, flake-utils, ... }:
     let
       supportedSystem = [ "x86_64-linux" "x86_64-darwin" ];
 
@@ -53,59 +53,54 @@
         "ghc901"
       ];
 
-      forAllSystems = f: nixpkgs.lib.genAttrs supportedSystem (system: f system);
-
       forAllGhcs = f: nixpkgs.lib.genAttrs supportedGhcVersions (ghc: f ghc);
 
       library = import ./lib.nix;
     in
-      {
-        build = forAllSystems (
-          system:
-            forAllGhcs (
-              ghcVersion: let
-                pkgs = import nixpkgs {
-                  config.allowUnfree = true;
-                  overlays = [ haskell-nix.overlay ];
-                  inherit system;
-                };
-              in
-                library { inherit pkgs system ghcVersion inputs; }
-            )
-        );
-
-        apps = forAllSystems (
-          system:
-            let
+      flake-utils.lib.eachSystem supportedSystem (
+        system: {
+          lib = forAllGhcs (
+            ghcVersion: let
               pkgs = import nixpkgs {
                 config.allowUnfree = true;
                 overlays = [ haskell-nix.overlay ];
                 inherit system;
               };
-              mkHlsMaterialization = ghcVersion: (library { inherit pkgs system ghcVersion inputs; }).project.plan-nix.passthru.generateMaterialized;
-              generateMaterializationBin = pkgs.writeShellScriptBin "generateMaterialization" ''
-                # This runs the 'updateMaterialize' script in all platform combinations we care about.
-                echo "Generating materialization for $1/hls-unstable-ghc865..."
-                ${mkHlsMaterialization "ghc865"} $1/hls-unstable-ghc865
-                echo "Generating materialization for $1/hls-unstable-ghc884..."
-                ${mkHlsMaterialization "ghc884"} $1/hls-unstable-ghc884
-                echo "Generating materialization for $1/hls-unstable-ghc8104..."
-                ${mkHlsMaterialization "ghc8104"} $1/hls-unstable-ghc8104
-                echo "Generating materialization for $1/hls-unstable-ghc8105..."
-                ${mkHlsMaterialization "ghc8105"} $1/hls-unstable-ghc8105
-                echo "Generating materialization for $1/hls-unstable-ghc8106..."
-                ${mkHlsMaterialization "ghc8106"} $1/hls-unstable-ghc8106
-                echo "Generating materialization for $1/hls-unstable-ghc8107..."
-                ${mkHlsMaterialization "ghc8107"} $1/hls-unstable-ghc8107
-                echo "Generating materialization for $1/hls-unstable-ghc901..."
-                ${mkHlsMaterialization "ghc901"}  $1/hls-unstable-ghc901
-              '';
-            in {
+            in
+              library { inherit pkgs system ghcVersion inputs; }
+          );
+
+          apps = let
+            pkgs = import nixpkgs {
+              config.allowUnfree = true;
+              overlays = [ haskell-nix.overlay ];
+              inherit system;
+            };
+            mkHlsMaterialization = ghcVersion: (library { inherit pkgs system ghcVersion inputs; }).project.plan-nix.passthru.generateMaterialized;
+            generateMaterializationBin = pkgs.writeShellScriptBin "generateMaterialization" ''
+              # This runs the 'updateMaterialize' script in all platform combinations we care about.
+              echo "Generating materialization for $1/hls-unstable-ghc865..."
+              ${mkHlsMaterialization "ghc865"} $1/hls-unstable-ghc865
+              echo "Generating materialization for $1/hls-unstable-ghc884..."
+              ${mkHlsMaterialization "ghc884"} $1/hls-unstable-ghc884
+              echo "Generating materialization for $1/hls-unstable-ghc8104..."
+              ${mkHlsMaterialization "ghc8104"} $1/hls-unstable-ghc8104
+              echo "Generating materialization for $1/hls-unstable-ghc8105..."
+              ${mkHlsMaterialization "ghc8105"} $1/hls-unstable-ghc8105
+              echo "Generating materialization for $1/hls-unstable-ghc8106..."
+              ${mkHlsMaterialization "ghc8106"} $1/hls-unstable-ghc8106
+              echo "Generating materialization for $1/hls-unstable-ghc8107..."
+              ${mkHlsMaterialization "ghc8107"} $1/hls-unstable-ghc8107
+              echo "Generating materialization for $1/hls-unstable-ghc901..."
+              ${mkHlsMaterialization "ghc901"}  $1/hls-unstable-ghc901
+            '';
+          in
+            {
               generateMaterialization = {
                 type = "app";
                 program = "${generateMaterializationBin}/bin/generateMaterialization";
               };
-            }
-        );
-      };
+            };
+        }
+      );
 }
