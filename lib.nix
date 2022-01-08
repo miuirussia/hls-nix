6d3d68430d82fd04228ba23df2e54d6e46e30b05
Kirill Kuznetsov <kirill.desirehd@gmail.com>
2022-01-08 20:21:50 +0300

    Add nonReinstallablePkgs

--- lib.nix
+++ lib.nix
@@ -58,7 +58,25 @@ let
 
   allExes = pkg: pkg.components.exes;
 
-  defaultModules = [ { enableSeparateDataOutput = true; } ];
+  defaultModules = [
+    { enableSeparateDataOutput = true; }
+    {
+      nonReinstallablePkgs = [
+        "rts" "ghc-heap" "ghc-prim" "integer-gmp" "integer-simple" "base"
+        "deepseq" "array" "ghc-boot-th" "pretty" "template-haskell"
+        # ghcjs custom package
+        "ghcjs-prim" "ghcjs-th"
+        "ghc-bignum" "exceptions" "stm"
+        "ghc-boot"
+        "ghc" "Cabal" "Win32" "array" "binary" "bytestring" "containers"
+        "directory" "filepath" "ghc-boot" "ghc-compact" "ghc-prim"
+        # "ghci" "haskeline"
+        "hpc"
+        "mtl" "parsec" "process" "text" "time" "transformers"
+        "unix" "xhtml" "terminfo"
+      ];
+    }
+  ];
 
   createProject = name:
     let
